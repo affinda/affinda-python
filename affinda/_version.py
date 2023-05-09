d4cb9df1560ef9ce0a20109ba1706c56d04cf302
Andrew Bird <andrewbirdemail@gmail.com>
2023-05-09 01:31:52 +0000

    #autocommit #release - 4.3.5

--- affinda/_version.py
+++ affinda/_version.py
@@ -4,4 +4,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "4.3.4"
+VERSION = "4.3.5"
