9044738a6f4bb74a2673a6b63ee4b996c9cef3e9
rockstarr-programmerr <rockstarrprogrammerr@gmail.com>
2023-09-21 03:59:45 +0000

    #autocommit #release - 4.10.0

--- affinda/_version.py
+++ affinda/_version.py
@@ -4,4 +4,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "4.9.0"
+VERSION = "4.10.0"
